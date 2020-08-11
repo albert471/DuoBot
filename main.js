55d246a34d0ebc5d372fcccb6ce37c8acc8ea4da
Alberto Checcone <alcheccone@gmail.com>
2020-08-10 17:45:03 -0700

    code cleanup+ fixed memory leak
    fixed scope on some variables, removed unneeded dependencies, general code cleanup

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // * Basic discord bot written in Javascript (Discord.js)
 // * When prompted, calculates duo and solo winrate for a given summoner
 // * @author: Albert471
-// * @version: 1.1.4
+// * @version: 1.1.6
 //=====================================================================================
 
 //todo:  add more features, bug test concurrency/other regions/edge cases/error cases/caching
@@ -13,15 +13,12 @@
 
 /** API related variables **/
 const api = ""; //Riot API key
-const apikey = `api_key=` + api; //alternative format
-
 const disctoken = ``; //Discord Token
 
 /** Libraries the bot requires **/
 const Discord = require(`discord.js`);
 const client = new Discord.Client();
 client.login(disctoken);
-const https = require('https');
 const fs = require(`fs`);
 const TeemoJS = require('teemojs');
 let tApi = TeemoJS(api);
@@ -42,16 +39,17 @@ const regionendpoint = {
 	"oce": "oc1",
 	"tr": "tr1",
 	"ru": "ru"
-}
+};
 
 /** League API-specific global variables **/
-const seasonsstartepoch = 1578488400; //start of season 10
+const seasonsstartepoch = 1578488400000; //start of season 10
 const clashqueue = 700;
 const flexqueue = 440;
 const soloduoqueue = 420;
 
 /** Other global variables **/
 let champjson = {};
+let matchcache = {};
 let globalReplyMessage = ``;
 let threshold = 3;
 let queue = 0; //number of items in queue
@@ -83,40 +81,40 @@ async function getCache() {
 returns an array with [accountid, summonerid]. **/
 async function getID(s,server) {
 	// pull account id
-	let dd = [];
+	let dataArray = [];
 	await tApi.get(server, 'summoner.getBySummonerName', s)
 	.then(data => {
 		if (data != null) {
-			dd = [data.accountId, data.id];
+			dataArray = [data.accountId, data.id];
 		}
-		return dd;
+		return dataArray;
 	})
 	.catch(err => {
-		dd = [];
+		dataArray = [];
 		console.log(err);
-		return dd;
+		return dataArray;
 	});
-	return dd;
+	return dataArray;
 }
 
 
-/** populates the mathistory array with the games played in QUEUE
+/** populates the matchistory array with the games played in QUEUE
 keep searching until nothing is returned **/
 async function getmatchhistory(queue,region, accountid) {
 	let matchhistory = [];
 	let counter = 0;
 	let numreturned = -1;
 	while (numreturned == -1 || numreturned > 0) {
-		await tApi.get(region, 'match.getMatchlist', accountid, { queue: queue, beginTime: 1578477600000, beginIndex: counter })
+		await tApi.get(region, 'match.getMatchlist', accountid, { queue: queue, beginTime: seasonsstartepoch, beginIndex: counter })
 		.then(data => {
 			counter += 100;
-			if (data == null || data == undefined || data['matches'] == undefined) {
+			if (data == null || data == undefined || data.matches == undefined) {
 				numreturned = 0;
 				return;
 			}
-			numreturned = data['matches'].length;
+			numreturned = data.matches.length;
 			for (let x=0; x < numreturned; x++) {
-				matchhistory.push(data['matches'][x]['gameId']);
+				matchhistory.push(data.matches[x].gameId);
 			}
 		});
 	}
@@ -135,7 +133,7 @@ async function getMatchInfo(matchid, region) {
 			matchcache[matchid] = data;
 			fs.writeFile(`matchcache.txt`, JSON.stringify(matchcache), (err) => 
 			{ if (err) throw err; });
-		})
+		});
 }
 
 /** calculates teammates and adds them to the teammates object. **/
@@ -143,38 +141,38 @@ function analyzeMatch(matchid, teammates, accountid) {
 	if (!matchcache[matchid]) {
 		return teammates;
 	}
-	if (matchcache[matchid]['gameDuration'] < 300) {
+	if (matchcache[matchid].gameDuration < 300) {
 		return teammates;
 	}
 	//find participantid of analyzed player by matching accountId
-	const partId = matchcache[matchid]['participantIdentities'];
+	const partId = matchcache[matchid].participantIdentities;
 	let foundId = partId.find(p => {
-		return p['player']['accountId'] == accountid;
-	})['participantId'];
+		return p.player.accountId == accountid;
+	}).participantId;
 	//find team of summoner
-	const part = matchcache[matchid]['participants'];
+	const part = matchcache[matchid].participants;
 	let teamId = part.find(pl => {
-		return pl['participantId'] == foundId;
-	})['teamId'];
+		return pl.participantId == foundId;
+	}).teamId;
 	//find teammates; pull partId using teammates[posn]['participantId']
 	let teammatesarr = [];
 	for (let y=0; y<part.length; y++) {
-		if (part[y]['teamId'] == teamId) {
-			teammatesarr.push(part[y]['participantId']);
+		if (part[y].teamId == teamId) {
+			teammatesarr.push(part[y].participantId);
 		}
 	}
 	//add them
 	for (let x=0; x < teammatesarr.length; x++) {
 		let teammatepid = teammatesarr[x];
 		let teammate = partId.find(pl => {
-			return pl['participantId'] == teammatepid && pl['player']['accountId'] != accountid;
+			return pl.participantId == teammatepid && pl.player.accountId != accountid;
 		});
 		if (teammate) {
-			teammate = teammate['player'];
-			if (!teammates[teammate['summonerName']]) {
-				teammates[teammate['summonerName']] = [];
+			teammate = teammate.player;
+			if (!teammates[teammate.summonerName]) {
+				teammates[teammate.summonerName] = [];
 			}
-			teammates[teammate['summonerName']].push(matchid);
+			teammates[teammate.summonerName].push(matchid);
 		}
 	}
 	return teammates;
@@ -184,23 +182,25 @@ function analyzeMatch(matchid, teammates, accountid) {
 function getWinOrLoss(matchid, accountid) {
 	if (!matchid in matchcache) return;
 	// get participantid of searched player
-	const partId = matchcache[matchid]['participantIdentities'];
+	const partId = matchcache[matchid].participantIdentities;
 	let foundId = partId.find(p => {
-		return p['player']['accountId'] == accountid;
-	})['participantId'];
+		return p.player.accountId == accountid;
+	}).participantId;
 	// get participant from participantid
-	const part = matchcache[matchid]['participants'];
+	const part = matchcache[matchid].participants;
 	return part.find(pl => {
-		return pl['participantId'] == foundId;
-	})['stats']['win'];
+		return pl.participantId == foundId;
+	}).stats.win;
 }
 
 /** returns true if the two matches have the same queue.  soft errors if it doesn't exist */
 function compareQueues(matchid1, matchid2) {
-	if (!matchid1 in matchcache || !matchid2 in matchcache) return;
-	let queue1 = matchcache[matchid1]["queueId"];
-	let queue2 = matchcache[matchid2]["queueId"];
-	return queue1 == queue2;
+      if (matchid1 in matchcache && matchid2 in matchcache) {
+	let queue1 = matchcache[matchid1].queueId;
+	let queue2 = matchcache[matchid2].queueId;
+      return queue1 == queue2; 
+    }
+      return false;
 }
 
 /** takes all duo games from the duo object, adds them all to duomatchhistory.  calculates % wr of
@@ -221,25 +221,25 @@ function finalanalysis(threshold, teammates, matchhistory, accountid) {
 	}
 	for (let x=0; x<allplayers.length; x++) {
 		if (teammates[allplayers[x]].length >= threshold) {
-			duogames = duogames.concat(teammates[allplayers[x]])
+			duogames = duogames.concat(teammates[allplayers[x]]);
 		}
 	}
-	duogamesUnique = duogames.filter(function(elem, pos) {
+	let duogamesUnique = duogames.filter(function(elem, pos) {
     	return duogames.indexOf(elem) == pos;
-    })
+    });
     for (let y=0; y<duogamesUnique.length; y++) {
     	if (getWinOrLoss(duogamesUnique[y], accountid)) {
     		duogameswon++;
     	}
     }
     //filter out remake games from matchhistory
-    matchtemp = matchhistory.filter(m => {
-    	return matchcache[m]['gameDuration'] > 300;
+    let matchtemp = matchhistory.filter(m => {
+    	return matchcache[m].gameDuration > 300;
     });
     matchhistory = matchtemp;
 
-    sologames = [];
-    sologameswon = 0;
+    let sologames = [];
+    let sologameswon = 0;
     //get sologames by finding games not in duo cache
     for (let z=0; z<matchhistory.length; z++) {
     	if (!duogamesUnique.includes(matchhistory[z])) {
@@ -252,7 +252,7 @@ function finalanalysis(threshold, teammates, matchhistory, accountid) {
     	}
     }
     Object.keys(duoers).forEach(key => {
-    	counter = 0;
+    	let counter = 0;
     	for (let b=0; b<duoers[key].length; b++) {
     		if (getWinOrLoss(duoers[key][b], accountid)) {
     			counter++;
@@ -261,9 +261,9 @@ function finalanalysis(threshold, teammates, matchhistory, accountid) {
     	duoers[key] = [counter, duoers[key].length];
     });
 
-    totalgamesplayed = matchhistory.length;
-    duogamesplayed = duogamesUnique.length;
-    sologamesplayed = sologames.length;
+    let totalgamesplayed = matchhistory.length;
+    let duogamesplayed = duogamesUnique.length;
+    let sologamesplayed = sologames.length;
     totalgamesplayed = sologamesplayed + duogamesplayed;
     return [duogameswon, duogamesplayed, sologameswon, sologamesplayed, totalgamesplayed, duoers];
 }
@@ -271,7 +271,7 @@ function finalanalysis(threshold, teammates, matchhistory, accountid) {
 /** Returns a timestamp for the given epoch **/
 function getTimeStamp(time)
 {
-	today = new Date(time);
+	let today = new Date(time);
 	let date = `${(today.getMonth() + 1)}-${today.getDate()}`;
 	//make it proper military time
 	let theHour = today.getHours();
@@ -289,7 +289,7 @@ function getTimeStamp(time)
 function insertionSort(array) {
   for (let i = 1; i < array.length; i++) {
     let j = i - 1;
-    let temp = array[i]
+    let temp = array[i];
     while (j >= 0 && matchcache[array[j]] < matchcache[temp]) { 
       array[j + 1] = array[j];
       j--;
@@ -320,19 +320,6 @@ function getChampFromId(id) {
 		return parseInt(champjson.data[c].key) == id;
 	})[0];
 }
-
-async function main() {	
-	await getmatchhistory(soloduoqueue);
-	for (let x=0; x < matchhistory.length; x++) {
-		await getMatchInfo(matchhistory[x]);
-	}
-	for (let y=0; y< matchhistory.length; y++) {
-		analyzeMatch(matchhistory[y]);
-	}
-	finalanalysis(3);
-}
-
-//main();
 
 /**               Client ready listener
     performs these commands when bot is turned on    **/
@@ -377,7 +364,7 @@ const onReady =
     console.log(`\n\n=-----------------------------------------------------------=`);
     console.log(`\nInformation will appear here when bot actions are triggered.`);
     console.log(`Bot loaded successfully`);
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
   },
   setPresence(RateLimited)
     {
@@ -386,19 +373,19 @@ const onReady =
     		let game = `with someone (Busy -- possible delay)    Help command: !duo help`;
     		client.user.setActivity(game, { type: `PLAYING` });
     	} else {
-            game = `TV (Free to handle requests)    Help command: !duo help`;
+            let game = `TV (Free to handle requests)    Help command: !duo help`;
             client.user.setActivity(game, { type: `WATCHING`});
     	}
     },
     async getReady() {
     	await loadChamps();
    		await getCache();
-   		console.log(`cachesize: ${Object.keys(matchcache).length}`)
+   		console.log(`cachesize: ${Object.keys(matchcache).length}`);
     }
-}
+};
 
 const onMessage =  {
-	async losers(receivedMessage, matchhistory, accountid, sentmsg) {
+	async losers(receivedMessage, matchhistory, accountid, sentmsg, summonerName) {
 		let totalstreakarray = [];
 		let totalwon = 0;
 		let remakecount = 0;
@@ -411,7 +398,7 @@ const onMessage =  {
 			let won = 0;
 			for (let x=matchhistory.length - 1; x >= 0; x--) {
 				let queuecheck = x < matchhistory.length - 1 ? compareQueues(matchhistory[x], matchhistory[x+1]) : true;
-				if (matchcache[matchhistory[x]]['gameDuration'] <= 300) {
+				if (matchcache[matchhistory[x]].gameDuration <= 300) {
 					remakecount++;
 					continue;
 				}
@@ -433,9 +420,6 @@ const onMessage =  {
 						longeststreak = currentstreak;
 					}
 				}
-				if (streak == 1) {
-				console.log(`${matchhistory[x]}:${matchcache[matchhistory[x]]["gameCreation"]}won:${wonThisOne}, currentstreak:${currentstreak}, totalcounted: ${totalcounted}, totalwon: ${totalwon}`);
-			}
 			}
 			totalstreakarray.push(won);
 			totalstreakarray.push(totalcounted);
@@ -444,15 +428,14 @@ const onMessage =  {
 		remakecount = remakecount / 4;
 		let totalwinrate = `${parseFloat(totalwon/(matchhistory.length - remakecount)*100).toFixed(2)}%`;
 		//analyzed matchhistory.length matches, found totalstreakarray[1,3,5,7] games respectively with winrates...
-		let color = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
 		const embed = createEmbed(summonerName,'Calculated Loss Streak Information');
 		embed.addFields(
-				{ name: 'All Ranked Games:', value: 'Played ' + (matchhistory.length - remakecount) + ', Won ' + totalwon + ", Winrate: " + totalwinrate},
-			)
-		for (x=1; x <=4; x++) {
+				{ name: 'All Ranked Games:', value: 'Played ' + (matchhistory.length - remakecount) + ', Won ' + totalwon + ", Winrate: " + totalwinrate}
+			);
+		for (let x=1; x <=4; x++) {
 			let gamesplayed = totalstreakarray[2*x-1];
 			let gameswon = totalstreakarray[2*x-2];
-			let winr = gamesplayed == 0 ? `N/A` : `${parseFloat(gameswon/gamesplayed * 100).toFixed(2)}%`
+			let winr = gamesplayed == 0 ? `N/A` : `${parseFloat(gameswon/gamesplayed * 100).toFixed(2)}%`;
 			embed.addField(`Games played on ${x}+ loss streak:`, `Played ${gamesplayed}, Won ${gameswon}, Winrate: ${winr}`, false);
 		}
 		embed.addField(`Longest losing streak in a ranked queue:`, longeststreak, true);
@@ -460,12 +443,12 @@ const onMessage =  {
 		sentmsg.edit(embed);
 
 	},
-	async duo(receivedMessage, matchhistory, accountid, sentmsg) {
+	async duo(receivedMessage, matchhistory, accountid, sentmsg, summonerName) {
     	let teammates = {};
     	for (let y=0; y < matchhistory.length; y++) {
 			teammates = analyzeMatch(matchhistory[y], teammates, accountid);
 		}
-		finalarr = finalanalysis(threshold, teammates, matchhistory, accountid);
+		let finalarr = finalanalysis(threshold, teammates, matchhistory, accountid);
 		//finalarr is [duos won, duos played, solos won, solos played, total played, duoers object]
 		let duowr = finalarr[0]/finalarr[1]*100;
 		let solowr = finalarr[2]/finalarr[3]*100;
@@ -476,17 +459,16 @@ const onMessage =  {
 		for (let x=0; x<winrateArray.length; x++) {
 			winrateArray[x] = Number.isNaN(winrateArray[x]) ? `N/A` : `${parseFloat(winrateArray[x]).toFixed(2)}%`;
 		}
-		let color = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
 		const embed = createEmbed(summonerName, `Calculated Duo Information`);
 		embed.addFields(
 				{ name: 'All Ranked Games:', value: 'Played ' + finalarr[4] + ', Won ' + totalwon + ", Winrate: " + winrateArray[2]},
 				{ name: 'Games With a Duo:', value: 'Played ' + finalarr[1] + ', Won ' + finalarr[0] + ", Winrate: " + winrateArray[0]},
 				{ name: 'Games Without a Duo:', value: 'Played ' + finalarr[3] + ', Won ' + finalarr[2] + ", Winrate: " + winrateArray[1]},
-				{ name: '\u200B', value: '\u200B' },
-			)
-		duoSorted = Object.keys(finalarr[5]).sort((a,b) => {
+				{ name: '\u200B', value: '\u200B' }
+			);
+		let duoSorted = Object.keys(finalarr[5]).sort((a,b) => {
 			return finalarr[5][b][1] - finalarr[5][a][1];
-		})
+		});
 		if (duoSorted.length > 0) {
 			embed.addField('Duos (minimum 3 games played together):',"Win-Loss records for each of your duos.", false);
 		}
@@ -511,8 +493,8 @@ const onMessage =  {
 	},
 	async lookupMsg(receivedMessage)
     {
-        message = receivedMessage.content;
-        summonerName = ``;
+        let message = receivedMessage.content;
+        let summonerName = ``;
         //use regex to find the region and summoner name
         let region = message.match(/(?<=!duo |!duolosers )[\w]+/i);
         if (!region || region.length != 1) {
@@ -529,7 +511,7 @@ const onMessage =  {
         }
         region = regionendpoint[region];
         let reg = /(?<=!duo [\w]+ |!duolosers [\w]+ )[àáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿąćęıłńœśšźżžƒÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞŸĄĆĘIŁŃŒŚŠŹŻŽªºßˆˇˉﬁﬂµμ\w\d\s]+/i;
-        summonerName = message.match(reg)
+        summonerName = message.match(reg);
         if (!summonerName || summonerName.length != 1) {
             receivedMessage.react(xmark);
             return false;
@@ -544,7 +526,6 @@ const onMessage =  {
         receivedMessage.channel.send(globalReplyMessage)
         .then(async (message) => {
         	let matchhistory = [];
-        	let sentMessage  = message;
         	joinQueue();
         	let ids = await getID(summonerName, region);
         	if (ids.length != 2) {
@@ -554,9 +535,9 @@ const onMessage =  {
         	}
         	let accountid = ids[0];
         	let summonerid = ids[1];
-        	solom = await getmatchhistory(soloduoqueue, region, accountid);
+        	let solom = await getmatchhistory(soloduoqueue, region, accountid);
         	solom = insertionSort(solom);
-        	flexm = await getmatchhistory(flexqueue, region, accountid);
+        	let flexm = await getmatchhistory(flexqueue, region, accountid);
         	flexm = insertionSort(flexm);
         	matchhistory = matchhistory.concat(solom);
         	matchhistory = matchhistory.concat(flexm);
@@ -578,14 +559,14 @@ const onMessage =  {
 
         	//send the matches off to the correct analysis function
         	if (receivedMessage.content.search(/^!duo /i) > -1) {
-        		onMessage.duo(receivedMessage, matchhistory, accountid, sentMessage);
+        		onMessage.duo(receivedMessage, matchhistory, accountid, message, summonerName);
         	} else if (receivedMessage.content.search(/^!duolosers /i) > -1) {
-        		onMessage.losers(receivedMessage, matchhistory, accountid, sentMessage);
+        		onMessage.losers(receivedMessage, matchhistory, accountid, message, summonerName);
         	} else {
         		message.react(xmark);
         	}
         	leaveQueue();
-        })
+        });
     },
     help(receivedMessage) {
     	globalReplyMessage = `Hi, I'm DuoBot.  If you would like to search your summoner, please use the following command: \n`;
@@ -593,7 +574,7 @@ const onMessage =  {
     	globalReplyMessage += `have questions, feedback, or a bug to report? Message me at APotS#8566 or join <https://discord.gg/zdAajBZ>.`;
     	receivedMessage.channel.send(globalReplyMessage);
     }
-}
+};
 
 /** Handles number of active threads. When count is 0, status changs to "not busy". **/
 function leaveQueue() {
